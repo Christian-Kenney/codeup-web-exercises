199420277c186889bb83a95066f8a59e205c1a2e
Christian Kenney <armyystrongg@Christians-MacBook-Air.local>
2018-07-17 12:16:13 -0500

    cleaned up code

--- js/promises.js
+++ js/promises.js
@@ -17,9 +17,7 @@ function getGithubUsernames() {
     return fetch('https://api.github.com/users/Christian-Kenney/events', {headers: {'Authorization': '3c6d033fdf077de45e7622b960aadbed880583fd'}})
         .then(response => response.json())
 }
-
 getGithubUsernames().then((usernames) => {
-
     console.log(usernames[0].created_at);
 }).catch(error => console.error(error));
 
